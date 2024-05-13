d15c775c2b5fa559ffded03e89182f37241e33f9
praveng <praveng1@gmail.com>
2024-05-13 23:36:58 +0300

    refactored yet again, + added apiv2 with 1 million domains

--- app.py
+++ app.py
@@ -4,71 +4,62 @@ import tldextract
 import os
 from gensim.models import KeyedVectors
 
-kv = KeyedVectors.load('domain_vectors.kv')
+# Load keyed vectors
+kv_1 = KeyedVectors.load('domain_vectors.kv')
+kv_2 = KeyedVectors.load('model_apiv2.kv')
 
 app = Flask(__name__)
 
 
-# Clean Domains
+# Extract registered domain
 def get_regd(url):
     ext = tldextract.extract(url)
     return ext.registered_domain
 
 
-def get_similar_domains(pos_domains, neg_domains, limit):
-
-    # Ensure that domains are included in the kv before querying
-    neg_domains = [x for x in neg_domains if x in kv.key_to_index.keys()]
-    pos_domains = [x for x in pos_domains if x in kv.key_to_index.keys()]
+def get_similar_domains(pos_domains, neg_domains, limit, model):
+    # Ensure that domain is in the model
+    neg_domains = [x for x in neg_domains if x in model.key_to_index.keys()]
+    pos_domains = [x for x in pos_domains if x in model.key_to_index.keys()]
 
     # Get similar domains
-    results = kv.most_similar(positive=pos_domains, negative=neg_domains, topn=limit)
+    results = model.most_similar(positive=pos_domains, negative=neg_domains, topn=limit) if pos_domains else []
 
-    formatted_results = [
-        {
-            "domain": x[0],
-            "score": round(float(x[1]), 4)
-        }
-        for x in results
-    ]
+    return [{"domain": x[0], "score": round(float(x[1]), 4)} for x in results]
 
-    return formatted_results
+
+def handle_request(model):
+    try:
+        # Parse domains
+        pos_domains = [get_regd(x) for x in request.args.get('pos_domains', '').split(',')[:10]] if request.args.get('pos_domains') else []
+        neg_domains = [get_regd(x) for x in request.args.get('neg_domains', '').split(',')[:10]] if request.args.get('neg_domains') else []
+
+        # Max limit = 100
+        limit = min(int(request.args.get('limit', 10)), 100)
+
+        # Early exit of no pos_domain
+        if not pos_domains:
+            return json.dumps({"similar": []}), 200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}
+
+        # Fetch similar domains
+        results = get_similar_domains(pos_domains, neg_domains, limit, model)
+
+        return json.dumps({"similar": results}), 200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}
+
+    except Exception as e:
+        # Print Error to logs
+        print(f'Error: {e}')
+        return str("Whoops! Something went wrong"), 500, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}
 
 
 @app.route('/apiv1')
-def main():
-    try:
-        pos_domains = []
-        neg_domains = []
-        limit = int(request.args.get('limit', 10))
+def apiv1():
+    return handle_request(kv_1)
 
-        if limit > 100:
-            limit = 100
 
-        # Parse out domains from query parameters, and strip out registered domains
-        if len(request.args.get('pos_domains', '')) > 0:
-            pos_domains = [x for x in request.args.get('pos_domains').split(',')][:10]
-            pos_domains = [get_regd(x) for x in pos_domains]
-
-        if len(request.args.get('neg_domains', '')) > 0:
-            neg_domains = [x for x in request.args.get('neg_domains').split(',')][:10]
-            neg_domains = [get_regd(x) for x in neg_domains]
-
-        if len(pos_domains) == 0:
-            return json.dumps({"similar": []}), 200, {"Content-Type": "application/json",
-                                                      "Access-Control-Allow-Origin": "*"}
-
-        # Query for similar domains
-        results = get_similar_domains(pos_domains, neg_domains, limit)
-
-        result_json = json.dumps({"similar": results})
-
-        return result_json, 200, {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}
-
-    except Exception as e:
-        print(e)
-        return str("Whoops! Something went wrong"), 400, {"Content-Type": "application/json",
-                                                          "Access-Control-Allow-Origin": "*"}
+@app.route('/apiv2')
+def apiv2():
+    return handle_request(kv_2)
 
 
 if __name__ == "__main__":
